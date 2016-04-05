4b1941f12bc2451483265983817037769a49cb87
Commodity Commodity <commodity@faggot.pw>
2016-04-05 23:48:49 +0100

    First version

---
+++ BinaryFractalTree/BinaryFractalTree/MainForm.Designer.cs
@@ -0,0 +1,403 @@
+﻿using System;
+
+namespace BinaryFractalTree
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.trackBar5 = new System.Windows.Forms.TrackBar();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.trackBar3 = new System.Windows.Forms.TrackBar();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.trackBar2 = new System.Windows.Forms.TrackBar();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.trackBar4 = new System.Windows.Forms.TrackBar();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).BeginInit();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar4)).BeginInit();
+            this.groupBox4.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1000, 657);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(1018, 617);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(234, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Generate";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(1018, 588);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(234, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Clear";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(1018, 646);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(234, 23);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Test";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(6, 19);
+            this.trackBar1.Maximum = 360;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(222, 45);
+            this.trackBar1.TabIndex = 4;
+            this.trackBar1.Value = 90;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(123, 52);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(19, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "90";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 52);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(34, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Angle";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label9);
+            this.groupBox1.Controls.Add(this.label10);
+            this.groupBox1.Controls.Add(this.trackBar5);
+            this.groupBox1.Controls.Add(this.checkBox2);
+            this.groupBox1.Controls.Add(this.checkBox1);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.trackBar3);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.trackBar2);
+            this.groupBox1.Location = new System.Drawing.Point(1018, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(234, 218);
+            this.groupBox1.TabIndex = 8;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "General";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(6, 156);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(50, 13);
+            this.label9.TabIndex = 19;
+            this.label9.Text = "Start size";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(123, 153);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(22, 13);
+            this.label10.TabIndex = 17;
+            this.label10.Text = "0.5";
+            // 
+            // trackBar5
+            // 
+            this.trackBar5.Location = new System.Drawing.Point(6, 121);
+            this.trackBar5.Minimum = 1;
+            this.trackBar5.Name = "trackBar5";
+            this.trackBar5.Size = new System.Drawing.Size(222, 45);
+            this.trackBar5.TabIndex = 18;
+            this.trackBar5.Value = 5;
+            this.trackBar5.Scroll += new System.EventHandler(this.trackBar5_Scroll);
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Checked = true;
+            this.checkBox2.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox2.Location = new System.Drawing.Point(9, 195);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(117, 17);
+            this.checkBox2.TabIndex = 16;
+            this.checkBox2.Text = "Redraw on change";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Checked = true;
+            this.checkBox1.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox1.Location = new System.Drawing.Point(9, 172);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(91, 17);
+            this.checkBox1.TabIndex = 15;
+            this.checkBox1.Text = "Clear on draw";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(6, 105);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(50, 13);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "Iterations";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(123, 102);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(13, 13);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "2";
+            // 
+            // trackBar3
+            // 
+            this.trackBar3.Location = new System.Drawing.Point(6, 70);
+            this.trackBar3.Minimum = 1;
+            this.trackBar3.Name = "trackBar3";
+            this.trackBar3.Size = new System.Drawing.Size(222, 45);
+            this.trackBar3.TabIndex = 13;
+            this.trackBar3.Value = 2;
+            this.trackBar3.Scroll += new System.EventHandler(this.trackBar3_Scroll);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 51);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(70, 13);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Size multiplier";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(123, 51);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(22, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "0.5";
+            // 
+            // trackBar2
+            // 
+            this.trackBar2.Location = new System.Drawing.Point(6, 19);
+            this.trackBar2.Maximum = 100;
+            this.trackBar2.Name = "trackBar2";
+            this.trackBar2.Size = new System.Drawing.Size(222, 45);
+            this.trackBar2.TabIndex = 10;
+            this.trackBar2.Value = 50;
+            this.trackBar2.Scroll += new System.EventHandler(this.trackBar2_Scroll);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.trackBar1);
+            this.groupBox2.Location = new System.Drawing.Point(1018, 236);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(234, 72);
+            this.groupBox2.TabIndex = 16;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Branch A";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.label7);
+            this.groupBox3.Controls.Add(this.label8);
+            this.groupBox3.Controls.Add(this.trackBar4);
+            this.groupBox3.Location = new System.Drawing.Point(1018, 314);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(234, 72);
+            this.groupBox3.TabIndex = 17;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Branch B";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(123, 52);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(25, 13);
+            this.label7.TabIndex = 5;
+            this.label7.Text = "270";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(6, 52);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(34, 13);
+            this.label8.TabIndex = 6;
+            this.label8.Text = "Angle";
+            // 
+            // trackBar4
+            // 
+            this.trackBar4.Location = new System.Drawing.Point(6, 19);
+            this.trackBar4.Maximum = 360;
+            this.trackBar4.Name = "trackBar4";
+            this.trackBar4.Size = new System.Drawing.Size(222, 45);
+            this.trackBar4.TabIndex = 4;
+            this.trackBar4.Value = 270;
+            this.trackBar4.Scroll += new System.EventHandler(this.trackBar4_Scroll);
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.textBox1);
+            this.groupBox4.Location = new System.Drawing.Point(1018, 392);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(234, 190);
+            this.groupBox4.TabIndex = 7;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "Log";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Consolas", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(6, 19);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(222, 165);
+            this.textBox1.TabIndex = 0;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1264, 681);
+            this.Controls.Add(this.groupBox4);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(1280, 720);
+            this.MinimumSize = new System.Drawing.Size(1280, 720);
+            this.Name = "MainForm";
+            this.Text = "Fractal Tree Generation - [lin-e]";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).EndInit();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar4)).EndInit();
+            this.groupBox4.ResumeLayout(false);
+            this.groupBox4.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TrackBar trackBar2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TrackBar trackBar3;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TrackBar trackBar4;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.TrackBar trackBar5;
+    }
+}
---
+++ BinaryFractalTree/BinaryFractalTree/MainForm.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace BinaryFractalTree
+{
+    public partial class MainForm : Form
+    {
+        Pen mainPen = new Pen(Color.Black, 2); // Declares a black pen with a thickness of 2
+        Graphics currentCanvas; // Declares the current drawing canvas
+        List<FractalPoint> allPoints = new List<FractalPoint>(); // Declares a list with all the points to draw
+        public static int fractalRuleA = 90; // Default rule for line A
+        public static int fractalRuleB = 270; // Default rule for line B
+        public static double sizeChangeRule = 0.5; // Default size change
+        public int iterationCount = 2; // Default iteration count
+        bool isDrawing = false; // Isn't currently drawing
+        public MainForm()
+        {
+            InitializeComponent();
+            currentCanvas = pictureBox1.CreateGraphics(); // Sets the current canvas to edit
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (isDrawing) // Only runs when the program is drawing
+            {
+                return; // Stops the flow of code at this point, to prevent issues with the list
+            }
+            isDrawing = true; // Starts drawing
+            allPoints.Clear(); // Resets the list
+            if (checkBox1.Checked) // Only runs if it's set to clear the image
+            {
+                pictureBox1.Image = null; // Clears the image
+                Helpers.Sleep(1); // Delays operation for 1ms
+            }
+            FractalPoint basePoint = new FractalPoint(this); // Declares the starting point
+            basePoint.currentBearing = 0; // Straight line
+            basePoint.originPoint = new Point(Convert.ToInt32(pictureBox1.Width / 2), 0); // Start point centred on canvas, at the base
+            basePoint.endPoint = new Point(Convert.ToInt32(pictureBox1.Width / 2), Convert.ToInt32(pictureBox1.Height / (1 / ((double)trackBar5.Value / (double)10)))); // Finds the location of the endpoint by dividing the canvas by the reciprocal of the base height setting
+            basePoint.lineLength = Convert.ToInt32(pictureBox1.Height / (1 / ((double)trackBar5.Value / (double)10))); // Calculates the line length, using the same logic as above
+            Draw(basePoint); // Draws the original point
+            allPoints.Add(basePoint); // Adds the base to the 'endpoints' list
+            for (int i = 0; i < iterationCount; i++) // Loops the specified number of times
+            {
+                FractalPoint[] pointCopy = allPoints.ToArray(); // Copies all the original points to an array
+                allPoints.Clear(); // Clears the list
+                foreach (FractalPoint singlePoint in pointCopy) // Does the following operations to each item in the list
+                {
+                    allPoints.Add(new FractalPoint(singlePoint, fractalRuleA)); // Adds a point with rule A
+                    allPoints.Add(new FractalPoint(singlePoint, fractalRuleB)); // Adds a point with rule B
+                }
+                foreach (FractalPoint toDraw in allPoints) // Loops through each point
+                {
+                    Draw(toDraw); // Draws said point
+                }
+            }
+            isDrawing = false; // Stops drawing
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Image = null; // Clears canvas
+            textBox1.Text = null; // Clears log
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            WriteLine("This doesn't do anything. I used it to test logic."); // Read it
+        }
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            fractalRuleA = trackBar1.Value; // Sets rule A to the settings value
+            label1.Text = fractalRuleA.ToString(); // Displays the new setting on the label
+            RedrawImage(); // Redraws the image
+        }
+        private void trackBar2_Scroll(object sender, EventArgs e)
+        {
+            sizeChangeRule = ((double)trackBar2.Value / (double)100); // Changes the size rule to allow for decimals
+            label4.Text = sizeChangeRule.ToString(); // Displays the new setting on the label
+            RedrawImage(); // Redraws the image
+        }
+        private void trackBar3_Scroll(object sender, EventArgs e)
+        {
+            iterationCount = trackBar3.Value; // Changes iteration depth
+            label6.Text = iterationCount.ToString(); // Displays the new setting on the label
+            RedrawImage(); // Redraws the image
+        }
+        private void trackBar4_Scroll(object sender, EventArgs e)
+        {
+            fractalRuleB = trackBar4.Value; // Sets rule B to the settings value
+            label7.Text = fractalRuleB.ToString(); // Displays the new setting on the label
+            RedrawImage(); // Redraws the image
+        }
+        private void trackBar5_Scroll(object sender, EventArgs e)
+        {
+            label10.Text = ((double)trackBar5.Value / (double)10).ToString(); // Sets the start length
+            RedrawImage(); // Redraws the image
+        }
+        private void RedrawImage()
+        {
+            if (checkBox2.Checked) // Only runs if it's set to redraw
+            {
+                button1_Click(this, null); // Runs the code in the button
+            }
+        }
+        private void Draw(FractalPoint toDraw)
+        {
+            currentCanvas.DrawLine(mainPen, new Point(toDraw.originPoint.X, pictureBox1.Height - toDraw.originPoint.Y), new Point(toDraw.endPoint.X, pictureBox1.Height - toDraw.endPoint.Y)); // Flips all points along the X axis as (0, 0) is at top left, versus the bottom left (as is conventional)
+        }
+        public void WriteLine(string toWrite)
+        {
+            textBox1.Text += toWrite + Environment.NewLine; // Appends the text, and a new line
+        }
+        public void WriteLine(string formatString, params string[] writeParams)
+        {
+            WriteLine(string.Format(formatString, writeParams)); // Formats the string and then writes it
+        }
+    }
+    public static class Helpers
+    {
+        public static double Degrees(this double inputDouble)
+        {
+            return inputDouble * (Math.PI / 180); // Converts from radians to degrees
+        }
+        public static double Degrees(this int inputDouble)
+        {
+            return Convert.ToDouble(inputDouble).Degrees(); // Same as above, albeit with int as the input value
+        }
+        public static void Sleep(int timeToSleep)
+        {
+            Stopwatch timeCount = new Stopwatch(); // Declares a new stopwatch
+            timeCount.Start(); // Starts said stopwatch
+            while (timeCount.ElapsedMilliseconds < timeToSleep) // Keeps running until time is up
+            {
+                Application.DoEvents(); // Prevents the program from freezing
+            }
+            timeCount.Stop(); // Stops said stopwatch
+        }
+    }
+    public class FractalPoint
+    {
+        public Point originPoint;
+        public Point endPoint;
+        public int currentBearing;
+        public int lineLength;
+        public MainForm parentForm;
+        public FractalPoint(FractalPoint originalPoint, int newBearing)
+        {
+            originPoint = originalPoint.endPoint; // Takes the origin point as the the endpoint of the parent point (trust me, this makes sense)
+            parentForm = originalPoint.parentForm; // Sets the parent form from the parent point
+            lineLength = Convert.ToInt32(originalPoint.lineLength * MainForm.sizeChangeRule); // Finds the length of this line
+            currentBearing = originalPoint.currentBearing + newBearing; // Sets the current bearing to be the original bearing added onto the rule
+            Calculate(); // Begins to calculate the location of the end point
+        }
+        public FractalPoint(MainForm parentForm)
+        {
+            this.parentForm = parentForm; // Sets the parent
+        }
+        private void Calculate()
+        {
+            int xAngle = (270 - currentBearing) * -1; // Finds the angle to determine the X coordinate
+            double xCos = Math.Cos(xAngle.Degrees()); // Works out the cosine of the angle
+            int xPoint = originPoint.X - Convert.ToInt32(xCos * lineLength); // Does the calculation for the X size, to calculate the new point
+            int yAngle = (90 - xAngle); // Finds the angle to determine the Y coordinate
+            double yCos = Math.Cos(yAngle.Degrees()); // Works out the cosine of the angle
+            int yPoint = originPoint.Y + Convert.ToInt32(yCos * lineLength); // Does the calculation for the Y size, to calculate the new point
+            endPoint = new Point(xPoint, yPoint); // Creates a new endpoint
+            parentForm.WriteLine("{0}, {1} [{2} {3}]", xPoint.ToString(), yPoint.ToString(), xAngle.ToString(), yAngle.ToString()); // Logs everything to the 'console'
+        }
+    }
+}
